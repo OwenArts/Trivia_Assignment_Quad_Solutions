7b923e89d60b300a46325ac07d6e252722f4ef32
Owen Arts <owen.arts@hotmail.com>
2026-03-30 21:35:34 +0200

    Moved gitignore file & added CORS

--- CSharp/Trivia Assignment backend/Program.cs
+++ CSharp/Trivia Assignment backend/Program.cs
@@ -2,11 +2,15 @@ using Microsoft.AspNetCore.ResponseCompression;
 using Trivia_Assignment_backend.Managers;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddHealthChecks();
+builder.Services.AddSingleton<HttpClient>();
+builder.Services.AddSingleton<QuestionSessionManager>();
 builder.Services.AddResponseCompression(options =>
 {
     options.EnableForHttps = true;
@@ -18,22 +22,27 @@ builder.Services.AddResponseCaching(options =>
     options.SizeLimit = 65536;
     options.UseCaseSensitivePaths = false;
 });
-builder.Services.AddHealthChecks();
-builder.Services.AddSingleton<HttpClient>();
-builder.Services.AddSingleton<QuestionSessionManager>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000/")
+                                .WithMethods("GET", "POST");
+                      });
+});
+
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.UseResponseCaching();
 app.UseResponseCompression();
+app.UseCors(MyAllowSpecificOrigins);
 
 app.Run();
